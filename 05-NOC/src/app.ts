edcb570b879741ef5e5e6b7c925b9390e74f3273
PaulReinosoA <164814520+PaulReinosoA@users.noreply.github.com>
2024-09-03 22:43:25 -0500

    clase de variables de  ambiente

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,9 +1,11 @@
 import { Server } from '../src/presentation/server';
+import { envs } from './config/plugins/envs.plugin';
 
 (async () => {
   await main(); //la ventaja es qeu podemos hacer que espere el await con la funcion autoinvocada
 })();
 
 function main() {
-  Server.start();
+  // Server.start();
+  console.log(envs);
 }
